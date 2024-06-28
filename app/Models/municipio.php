f472fe05825627d5c343f42c856ef848b1dcedc0
Alexander <desarrollador1@arsuarez.com>
2024-06-28 15:50:35 -0500

    Se realiza cambio de nombre a Municipio por error de clase

--- app/Models/municipio.php
+++ app/Models/municipio.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class municipio extends Model
+class Municipio extends Model
 {
     use HasFactory;
     protected $fillable = ['nombre','departamento_id', 'codigodivipola', 'numeromunicipio', 'estado'];
