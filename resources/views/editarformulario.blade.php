a522322307bf8dbb72dbeb9bbcda83ca2d8252a7
Alexander <desarrollador1@arsuarez.com>
2024-07-13 10:28:27 -0500

    se realiza actualizacion de la edicion de formulario ocultando proceso de contrasenia

--- resources/views/editarformulario.blade.php
+++ resources/views/editarformulario.blade.php
@@ -3,183 +3,204 @@
 @section('titulo' ,'Lista de Usuarios')
 
 @section('contenido')
-  
-  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.bundle.min.js"></script>
 
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.bundle.min.js"></script>
 
-  <div class="container">
+<div class="container">
     <div class="row">
-      <div class="col-md-12 text-center">
-        <h2 class="mt-2">Edición de Usuario</h2>
-      </div>
+        <div class="col-md-12 text-center">
+            <h2 class="mt-2">Edición de Usuario</h2>
+        </div>
     </div>
     <div id="mensaje-guardado" class="alert alert-success" style="display: none;" role="alert">
-      ¡Tu formulario ha sido guardado exitosamente!
+        ¡Tu formulario ha sido guardado exitosamente!
     </div>
     <form action="{{ route('usuario.update', $usuario->id)}}" method="POST" id="formu">
-      @csrf
+        @csrf
 
-      <div class="row">
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="numerodedocumento" class="form-label">N° de Documento</label>
-          <input type="number" value="{{ old('numerodedocumento', $usuario->numerodedocumento) }}" id="numerodedocumento" name="numerodedocumento" class="form-control @error('numerodedocumento') is-invalid @enderror" disabled>
-          @error('numerodedocumento')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
+        <div class="row">
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="numerodedocumento" class="form-label">N° de Documento</label>
+                <input type="number" value="{{ old('numerodedocumento', $usuario->numerodedocumento) }}" id="numerodedocumento" name="numerodedocumento" class="form-control @error('numerodedocumento') is-invalid @enderror" disabled>
+                @error('numerodedocumento')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="usuario" class="form-label">Usuario</label>
+                <input type="text" value="{{ old('usuario', $usuario->usuario) }}" id="usuario" name="usuario" class="form-control @error('usuario') is-invalid @enderror" disabled>
+                @error('usuario')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="nombre1" class="form-label">Primer Nombre</label>
+                <input type="text" value="{{ old('nombre1', $usuario->nombre1) }}" id="nombre1" name="nombre1" class="form-control @error('nombre1') is-invalid @enderror">
+                @error('nombre1')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="nombre2" class="form-label">Segundo Nombre</label>
+                <input type="text" value="{{ old('nombre2', $usuario->nombre2) }}" id="nombre2" name="nombre2" class="form-control @error('nombre2') is-invalid @enderror">
+                @error('nombre2')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="apellido1" class="form-label">Primer Apellido</label>
+                <input type="text" value="{{ old('apellido1', $usuario->apellido1) }}" id="apellido1" name="apellido1" class="form-control @error('apellido1') is-invalid @enderror">
+                @error('apellido1')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="apellido2" class="form-label">Segundo Apellido</label>
+                <input type="text" value="{{ old('apellido2', $usuario->apellido2) }}" id="apellido2" name="apellido2" class="form-control @error('apellido2') is-invalid @enderror">
+                @error('apellido2')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="telefono" class="form-label">Teléfono</label>
+                <input type="text" value="{{ old('telefono', $usuario->telefono) }}" id="telefono" name="telefono" class="form-control @error('telefono') is-invalid @enderror">
+                @error('telefono')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" value="{{ old('email', $usuario->email) }}" id="email" name="email" class="form-control @error('email') is-invalid @enderror" readonly>
+                @error('email')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="departamento" class="form-label">Departamento</label>
+                <select name="departamento" id="departamento" class="form-control @error('departamento') is-invalid @enderror">
+                    <option value="">Selecciona un departamento</option>
+                    @foreach ($departamentos as $departamento)
+                        <option value="{{ $departamento->id }}" {{ old('departamento', $usuario->departamento) == $departamento->id ? 'selected' : '' }}>{{ $departamento->nombre }}</option>
+                    @endforeach
+                </select>
+                @error('departamento')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="municipio" class="form-label">Municipio</label>
+                <select name="municipio" id="municipio" class="form-control @error('municipio') is-invalid @enderror">
+                    <option value="">Selecciona un municipio</option>
+                </select>
+                @error('municipio')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
+                <label for="rol" class="form-label">Roles</label>
+                <select name="rol" id="rol" class="form-control @error('rol') is-invalid @enderror">
+                    <option value="">Selecciona un rol</option>
+                    @foreach ($roles as $rol)
+                        <option value="{{ $rol->id }}" {{ old('rol', $usuario->rol) == $rol->id ? 'selected' : '' }}>{{ $rol->nombre }}</option>
+                    @endforeach
+                </select>
+                @error('rol')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col-12">
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="cambiar_contrasena" name="cambiar_contrasena">
+                    <label class="form-check-label" for="cambiar_contrasena">Cambiar Contraseña</label>
+                </div>
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6 contraseña-campos" style="display:none;">
+                <label for="contrasena" class="form-label">Contraseña</label>
+                <input type="password" id="contrasena" name="contrasena" class="form-control @error('contrasena') is-invalid @enderror">
+                @error('contrasena')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6 contraseña-campos" style="display:none;">
+                <label for="confirmacion_contrasena" class="form-label">Confirmación Contraseña</label>
+                <input type="password" id="confirmacion_contrasena" name="confirmacion_contrasena" class="form-control @error('confirmacion_contrasena') is-invalid @enderror">
+                @error('confirmacion_contrasena')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mt-3 col-12">
+                <button type="submit" class="btn btn-primary">Guardar</button>
+                <a href="../listado" class="btn btn-warning text-white">Atrás</a>
+            </div>
+
         </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="usuario" class="form-label">Usuario</label>
-          <input type="text" value="{{ old('usuario', $usuario->usuario) }}" id="usuario" name="usuario" class="form-control @error('usuario') is-invalid @enderror" disabled>
-          @error('usuario')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="nombre1" class="form-label">Primer Nombre</label>
-          <input type="text" value="{{ old('nombre1', $usuario->nombre1) }}" id="nombre1" name="nombre1" class="form-control @error('nombre1') is-invalid @enderror">
-          @error('nombre1')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="nombre2" class="form-label">Segundo Nombre</label>
-          <input type="text" value="{{ old('nombre2', $usuario->nombre2) }}" id="nombre2" name="nombre2" class="form-control @error('nombre2') is-invalid @enderror">
-          @error('nombre2')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="apellido1" class="form-label">Primer Apellido</label>
-          <input type="text" value="{{ old('apellido1', $usuario->apellido1) }}" id="apellido1" name="apellido1" class="form-control @error('apellido1') is-invalid @enderror">
-          @error('apellido1')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="apellido2" class="form-label">Segundo Apellido</label>
-          <input type="text" value="{{ old('apellido2', $usuario->apellido2) }}" id="apellido2" name="apellido2" class="form-control @error('apellido2') is-invalid @enderror">
-          @error('apellido2')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="telefono" class="form-label">Teléfono</label>
-          <input type="text" value="{{ old('telefono', $usuario->telefono) }}" id="telefono" name="telefono" class="form-control @error('telefono') is-invalid @enderror">
-          @error('telefono')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="email" class="form-label">Email</label>
-          <input type="text" value="{{ old('email', $usuario->email) }}" id="email" name="email" class="form-control @error('email') is-invalid @enderror" readonly>
-          @error('email')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="departamento" class="form-label">Departamento</label>
-          <select name="departamento" id="departamento" class="form-control @error('departamento') is-invalid @enderror">
-            <option value="">Selecciona un departamento</option>
-            @foreach ($departamentos as $departamento)
-            <option value="{{ $departamento->id }}" {{ old('departamento', $usuario->departamento) == $departamento->id ? 'selected' : '' }}>{{ $departamento->nombre }}</option>
-            @endforeach
-          </select>
-          @error('departamento')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="municipio" class="form-label">Municipio</label>
-          <select name="municipio" id="municipio" class="form-control @error('municipio') is-invalid @enderror">
-            <option value="">Selecciona un municipio</option>
-          </select>
-          @error('municipio')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="contrasena" class="form-label">Contraseña</label>
-          <input type="password" id="contrasena" name="contrasena" class="form-control @error('contrasena') is-invalid @enderror">
-          @error('contrasena')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="confirmacion_contrasena" class="form-label">Confirmación Contraseña</label>
-          <input type="password" id="confirmacion_contrasena" name="confirmacion_contrasena" class="form-control @error('confirmacion_contrasena') is-invalid @enderror">
-          @error('confirmacion_contrasena')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3 col col-12 col-sm-12 col-md-6 col-lg-6">
-          <label for="rol" class="form-label">Roles</label>
-          <select name="rol" id="rol" class="form-control @error('rol') is-invalid @enderror">
-            <option value="">Selecciona un rol</option>
-            @foreach ($roles as $rol)
-            <option value="{{ $rol->id }}" {{ old('rol', $usuario->rol) == $rol->id ? 'selected' : '' }}>{{ $rol->nombre }}</option>
-            @endforeach
-          </select>
-          @error('rol')
-          <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-
-        <div class="mt-3  col-12">
-          <button type="submit" class="btn btn-primary">Guardar</button>
-          <a href="../listado" class="btn btn-warning text-white">Atrás</a>
-        </div>
-
-      </div>
     </form>
-  </div>
+</div>
 
-  <script>
-      $(document).ready(function() {
+<script>
+    $(document).ready(function() {
         $('#departamento').on('change', function() {
-          var departamentoId = $(this).val();
-          if (departamentoId) {
-            $.ajax({
-              url: '../../departamentos/' + departamentoId + '/municipios',
-              type: "GET",
-              dataType: "json",
-              success: function(data) {
+            var departamentoId = $(this).val();
+            if (departamentoId) {
+                $.ajax({
+                    url: '../../departamentos/' + departamentoId + '/municipios',
+                    type: "GET",
+                    dataType: "json",
+                    success: function(data) {
+                        $('#municipio').empty();
+                        $('#municipio').append('<option value="">Selecciona un municipio</option>');
+                        $.each(data, function(key, value) {
+                            $('#municipio').append('<option value="' + value.id + '">' + value.nombre + '</option>');
+                        });
+
+                        // Seleccionar el municipio actual si existe
+                        var selectedMunicipio = '{{ old("municipio", $usuario->municipio) }}';
+                        $('#municipio').val(selectedMunicipio);
+                    }
+                });
+            } else {
                 $('#municipio').empty();
                 $('#municipio').append('<option value="">Selecciona un municipio</option>');
-                $.each(data, function(key, value) {
-                  $('#municipio').append('<option value="' + value.id + '">' + value.nombre + '</option>');
-                });
-
-                // Seleccionar el municipio actual si existe
-                var selectedMunicipio = '{{ old("municipio", $usuario->municipio) }}';
-                $('#municipio').val(selectedMunicipio);
-              }
-            });
-          } else {
-            $('#municipio').empty();
-            $('#municipio').append('<option value="">Selecciona un municipio</option>');
-          }
+            }
         });
 
         // Disparar el cambio para cargar los municipios cuando se carga la página si hay un departamento seleccionado
         var initialDepartamento = '{{ old("departamento", $usuario->departamento) }}';
         if (initialDepartamento) {
-          $('#departamento').val(initialDepartamento).trigger('change');
+            $('#departamento').val(initialDepartamento).trigger('change');
         }
-      });
-    </script>
 
+        // Mostrar u ocultar los campos de contraseña según el checkbox
+        $('#cambiar_contrasena').on('change', function() {
+            if ($(this).is(':checked')) {
+                $('.contraseña-campos').show();
+            } else {
+                $('.contraseña-campos').hide();
+            }
+        });
+
+        // Si hay un error de validación en los campos de contraseña, mostrar los campos
+        @if ($errors->has('contrasena') || $errors->has('confirmacion_contrasena'))
+            $('#cambiar_contrasena').prop('checked', true);
+            $('.contraseña-campos').show();
+        @endif
+    });
+</script>
 
 @endsection
+
